a888479534be5b603d4b1e85a15b8bbafb22eba1
Daniel Wehrle <dj.wehrle@gmail.com>
2017-11-04 11:42:30 -0500

    Initial Project Setup

---
+++ NetworkNeighbors/NetworkNeighbors/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NetworkNeighbors
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ NetworkNeighbors/NetworkNeighbors/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NetworkNeighbors.Startup))]
+namespace NetworkNeighbors
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
